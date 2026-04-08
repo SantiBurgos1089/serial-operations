2f1052448b0ba1098ea2be8bee58ccabed36aade
Santiago Burgos <santiago.burgos1089@gmail.com>
2026-04-08 16:07:28 -0600

    changed controls of the websocket page to being shown for sections instead of side by side

--- usr/lib/serialoper/websocket_page.py
+++ usr/lib/serialoper/websocket_page.py
@@ -11,12 +11,12 @@ except(ValueError, ImportError):
     gi.require_version('Adw', '1')
     from gi.repository import Adw
 
-from common.settings_app import GeneralSettings
-from common.serial_operations import SerialWebsocket
+import common.serial_operations
+import common.settings_app
 
 # Variables adicionales
-genset = GeneralSettings()
-serws = SerialWebsocket()
+genset = common.settings_app.GeneralSettings()
+serws = common.serial_operations.SerialWebsocket()
 
 # Configuracion de la seccion central donde iran todas las subsecciones
 class WebsocketPage(Adw.NavigationPage):
@@ -43,19 +43,161 @@ class WebsocketPage(Adw.NavigationPage):
         self.websocket_page = Adw.PreferencesPage()
         self.websocket_page.set_title("Websocket RS232")
 
+        # Serial port section
         self.serial_group = Adw.PreferencesGroup()
         self.serial_group.set_title("Configuracion puerto serial")
 
+        # ComboRow para puertos disponibles
+        self.rsport_row = Adw.ComboRow()
+        self.rsport_row.set_title("Puertos disponibles")
+        self.rsport_row.set_subtitle("Seleccione puerto del listado")
+        self.available_ports = genset.get_serial_ports()
+        self.rsport_string_list = Gtk.StringList.new()
+        for port in self.available_ports:
+            self.rsport_string_list.append(port)
+        self.rsport_row.set_model(self.rsport_string_list)
+
+        # Add row to section
+        self.serial_group.add(self.rsport_row)
+
+        # ComboRow para baudrate
+        # Se deja 9600 por defecto por ser el valor mas utilizado en equipos
+        self.baudrate_row = Adw.ComboRow()
+        self.baudrate_row.set_title("Baudrate")
+        self.baudrate_row.set_subtitle("Si no conoce valor puede dejar el valor 9600")
+        self.baudrate_string_list = Gtk.StringList.new()
+        for baudrate in common.serial_operations.BAUDRATE:
+            self.baudrate_string_list.append(baudrate)
+        self.baudrate_row.set_model(self.baudrate_string_list)
+        self.baudrate_row.set_selected(6)
+
+        # Add row to section
+        self.serial_group.add(self.baudrate_row)
+
+        # ComboRow para data bits
+        self.databits_row = Adw.ComboRow()
+        self.databits_row.set_title("Data bits")
+        self.databits_row.set_subtitle("Si no conoce valor puede dejar el valor 8")
+        self.databits_string_list = Gtk.StringList.new()
+        for databits in common.serial_operations.DATA_BITS:
+            self.databits_string_list.append(databits)
+        self.databits_row.set_model(self.databits_string_list)
+        self.databits_row.set_selected(3)
+
+        # Add row to section
+        self.serial_group.add(self.databits_row)
+
+        # ComboRow para paridad
+        self.parity_row = Adw.ComboRow()
+        self.parity_row.set_title("Paridad")
+        self.parity_string_list = Gtk.StringList.new()
+        for parity in common.serial_operations.PARITY:
+            self.parity_string_list.append(parity)
+        self.parity_row.set_model(self.databits_string_list)
+
+        # Add row to section
+        self.serial_group.add(self.parity_row)
+
+        # ComboRow para bits de parada
+        self.stopbits_row = Adw.ComboRow()
+        self.stopbits_row.set_title("Bit de parada")
+        self.stopbits_string_list = Gtk.StringList.new()
+        for stopbits in common.serial_operations.STOP_BITS:
+            self.stopbits_string_list.append(stopbits)
+        self.stopbits_row.set_model(self.stopbits_string_list)
+
+        # Add row to section
+        self.serial_group.add(self.stopbits_row)
+
+        # ComboRow para control de flujo
+        self.flowcontrol_row = Adw.ComboRow()
+        self.flowcontrol_row.set_title("Control de flujo")
+        self.flowcontrol_string_list = Gtk.StringList.new()
+        for flowcontrol in common.serial_operations.FLOW_CONTROL:
+            self.flowcontrol_string_list.append(flowcontrol)
+        self.flowcontrol_row.set_model(self.flowcontrol_string_list)
+
+        # Add row to section
+        self.serial_group.add(self.flowcontrol_row)
+
+        # Row para timeout
+        self.timeout_row = Adw.ActionRow()
+        self.timeout_row.set_title("Timeout")
+        self.timeout_row.set_subtitle("Tiempo de espera en segundos")
+        self.time_scale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 0.0, 5.0, 0.5)
+        self.time_scale.set_value(1)
+        self.time_scale.set_hexpand(True)
+        self.time_scale.set_draw_value(True)
+        self.timeout_row.add_suffix(self.time_scale)
+
+        # Add row to section
+        self.serial_group.add(self.timeout_row)
+
+        # Add section with all controls to page
+        self.websocket_page.add(self.serial_group)
 
 
 
 
 
+
+        # Websocket section
         self.websocket_group = Adw.PreferencesGroup()
         self.websocket_group.set_title("Configuracion Websocket")
 
+        # ComboRow para direccion IP del equipo y como mostrarse
+        self.ipport_row = Adw.ComboRow()
+        self.ipport_row.set_title("Direccion IP")
+        #self.ipport_row.set_subtitle("Seleccione como mostrar su equipo")
+        self.local_ip = genset.get_local_ip()
+        self.ipport_string_list = Gtk.StringList.new([
+            self.local_ip, 
+            "127.0.0.1", 
+            "0.0.0.0"])
+        self.ipport_row.set_model(self.ipport_string_list)
 
-        
+        # Add row to section
+        self.websocket_group.add(self.ipport_row)
+
+        # ComboRow para puerto de red del websocket a configurar
+        self.wsport_row = Adw.ComboRow()
+        self.wsport_row.set_title("Puerto de red a utilizar WebSocket")
+        self.wsport_string_list = Gtk.StringList.new()
+        for wsport in common.serial_operations.WEBSOCKET_PORT:
+            self.wsport_string_list.append(wsport)
+        self.wsport_row.set_model(self.wsport_string_list)
+        self.wsport_row.set_selected(0)
+
+        # Add row to section
+        self.websocket_group.add(self.wsport_row)
+
+        # Row para proceso de inicio websocket
+        self.process_row = Adw.ActionRow()
+        self.process_row.set_title("Gestiona el inicio y finalizacion del websocket")
+        self.process_row.set_subtitle("Iniciar WebSocket")
+        self.data_label = Gtk.Label()
+        self.data_label.set_label("Lectura WS: 0")
+        self.process_button = Gtk.Button()
+        #self.process_button.set_label("Iniciar WebSocket")
+        self.process_button.set_icon_name("xsi-media-playback-start-symbolic")
+        #self.process_button.connect("clicked", self.ws_log_data)
+        self.process_row.add_suffix(self.data_label)
+        self.process_row.add_suffix(self.process_button)
+
+        # Add row to section
+        self.websocket_group.add(self.process_row)
+
+        # Add section with all controls to page
+        self.websocket_page.add(self.websocket_group)
+
+
+
+
+
+
+
+
+
 
         # Seccion de contenido donde ira cada subseccion
         self.ws_content_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=12)
@@ -181,6 +323,7 @@ class WebsocketPage(Adw.NavigationPage):
         self.ws_content_box.append(self.ws_rightbox_section)
 
         # Añadiendo la seccion de contenido a la seccion central
+        self.ws_central_box.append(self.websocket_page)
         self.ws_central_box.append(self.ws_content_box)
 
         # Añadiendo la cabecera y contenido
